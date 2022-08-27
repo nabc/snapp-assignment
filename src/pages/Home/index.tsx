8a9f2afbbfc4b50260bfed572dece58b15001620
nabc <nima.abbc@gmail.com>
2022-08-27 16:36:02 +0430

    feat: install required packages and create essential backbone of project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  // TODO: should get contact list via api call
+
+  // TODO: should have a form - single input with throttling/debouncing - for filtering
+
+  // TODO: should handle state of loading
+  // TODO: should handle state of no data
+  // TODO: should show contact list if data is successfully retrieved
+  // TODO: contact list should use contact card component which is linked to the contact page
+
+  return <div>home page</div>;
+}
